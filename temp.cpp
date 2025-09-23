2c5621ced7b59476b8a29e502b15b22de2b5bf67
l-m-n-d-k <dima2642007@gmail.com>
2025-09-23 19:07:45 +0300

    firts + second seminar

---
+++ temp.cpp
@@ -0,0 +1,13 @@
+//task 2.2
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  double x, y, b;
+  cout << "write x, y, and b" << endl;
+  cin >> x >> y >> b;
+  
+
+  return 0;
+}
