e28d5ea0d2311e6430e7d7abea32c64a3ea82336
l-m-n-d-k <dima2642007@gmail.com>
2025-11-21 00:46:00 +0300

    upd practice-4

---
+++ practice-4/4_2.cpp
@@ -0,0 +1,84 @@
+//task 4_2
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "##########################################################" << endl;
+  cout << "# * * * * * * * *                                        #" << endl;
+  cout << "#  * * * * * * * *                                       #" << endl;
+  cout << "# * * * * * * * *                                        #" << endl;
+  cout << "#  * * * * * * * *                                       #" << endl;
+  cout << "# * * * * * * * *                                        #" << endl;
+  cout << "#  * * * * * * * *                                       #" << endl;
+  cout << "##########################################################" << endl;
+  cout << "#                                                        #" << endl;
+  cout << "##########################################################" << endl;
+  cout << "#                                                        #" << endl;
+  cout << "##########################################################" << endl;
+  cout << "#                                                        #" << endl;
+  cout << "##########################################################" << endl;
+  cout << "#                                                        #" << endl;
+  cout << "##########################################################" << endl;
+  cout << "#                                                        #" << endl;
+  cout << "##########################################################" << endl;
+  cout << "#                                                        #" << endl;
+  cout << "##########################################################" << endl;
+
+  return 0;
+}
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int WIDTH = 48;
+
+    cout << " ";
+    for (int i = 0; i < WIDTH; i++) {
+        cout << "_";
+    }
+    cout << endl;
+
+    for (int row = 0; row < 6; row++) {
+        cout << "|";
+
+        for (int col = 0; col < 8; col++) {
+            if (row % 2 == 0) {
+                cout << "* ";
+            } else {
+                cout << " *";
+            }
+        }
+
+        for (int i = 0; i < WIDTH - 16; i++) {
+            if (row % 2 == 0) {
+                cout << "#";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << "|" << endl;
+    }
+
+    for (int stripe = 0; stripe < 7; stripe++) {
+        cout << "|";
+        for (int i = 0; i < WIDTH; i++) {
+            if (stripe % 2 == 0) {
+                cout << "#";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << "|" << endl;
+    }
+
+    cout << " ";
+    for (int i = 0; i < WIDTH; i++) {
+        cout << "_";
+    }
+    cout << endl;
+
+    return 0;
+}
