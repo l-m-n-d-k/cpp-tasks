e5a7da6450cef0c5bf3728aebfb164349e44ab6d
l-m-n-d-k <dima2642007@gmail.com>
2025-12-06 14:47:56 +0300

    2_6 + 4_6

---
+++ practice-2/2_6.cpp
@@ -0,0 +1,18 @@
+//task 2.5
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+  double S, m, n, r;
+  r = 0.0001;
+  res = 0;
+  int p;
+  cin >> S >> m >> n;
+  while(res < m){
+    res = (S * r * pow(1 + r, n)) / (12 * (pow(1 + r, n) - 1));
+    r += 0.0001;
+  }
+  cout << r * 100 << endl;
+  return 0;
+}
